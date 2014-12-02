1fe7330ab782385913f18b957259d9ee8ec6fb85
Ilkorn <korneev.box@gmail.com>
2014-12-02 16:09:39 +0300

    FOOTER| fix responsive

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,12 @@
-console.log('\'Allo \'Allo!');
+(function(){
+    var SCREEN_SMALL = 768;
+    $(window).on('resize load', function(){
+        if($(window).width() < SCREEN_SMALL - 1){
+            $(".footer-collapsed").removeClass('in');
+        } else {
+            $(".footer-collapsed").addClass('in');
+        }
+    });
+
+
+})();
