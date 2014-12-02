5b13669cc48d0181b365214f7f16ff30ad907d49
Ilkorn <korneev.box@gmail.com>
2014-12-02 19:01:54 +0300

    redesign header

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,7 @@
 (function(){
     var SCREEN_SMALL = 768;
     $(window).on('resize load', function(){
+        // footer text if medium screen
         if($(window).width() < SCREEN_SMALL - 1){
             $(".footer-collapsed").removeClass('in');
         } else {
@@ -8,5 +9,20 @@
         }
     });
 
+    $('.nav-pills > li ').hover( function(){
+        if($(this).hasClass('hoverblock')){
+            $(this).siblings().removeClass('active');
+            $(this).addClass('active');
+            return;
+        } else {
+            $(this).find('a').tab('show');
+        }
+    });
+
+//
+//    $('.nav-tabs > li').find('a').click( function(){
+//        $(this).parent()
+//            .siblings().addClass('hoverblock');
+//    });
 
 })();
