e673243fa535b3f0eddb00758299ad00562850f5
Ilkorn <korneev.box@gmail.com>
2014-12-03 14:37:51 +0300

    fix small header

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,15 +1,41 @@
 (function(){
     var SCREEN_SMALL = 768;
-    $(window).on('resize load', function(){
+    $(window).on('resize load', showDecollapsedMenu);
+
+    $('.nav-pills > li ').hover(correctNavigationWithTabMenu);
+
+    $('#clientType').find('a').on('click', changeNavigationIfClientTypeChanged);
+
+    $('#selectLocation').click(function(event){
+
+        event.stopPropagation();
+//       $(this).preventDefault();
+    });
+
+    function showDecollapsedMenu(){
         // footer text if medium screen
         if($(window).width() < SCREEN_SMALL - 1){
             $(".footer-collapsed").removeClass('in');
         } else {
             $(".footer-collapsed").addClass('in');
         }
-    });
+    }
 
-    $('.nav-pills > li ').hover( function(){
+    function changeNavigationIfClientTypeChanged(event, item){
+        $('#clientType').find('a').removeClass('active');
+        $(this).addClass('active');
+        $(this).blur();
+
+        if ($(this).attr('id') == 'typeChef') {
+            $('.customer-only').addClass('hide');
+            $('.chefs-only').removeClass('hide');
+        } else {
+            $('.chefs-only').addClass('hide');
+            $('.customer-only').removeClass('hide');
+        }
+    }
+
+    function correctNavigationWithTabMenu(){
         if($(this).hasClass('hoverblock')){
             $(this).siblings().removeClass('active');
             $(this).addClass('active');
@@ -17,11 +43,9 @@
         } else {
             $(this).find('a').tab('show');
         }
-    });
-    $('#clientType').find('a').on('click', function(event, item){
-        $('#clientType').find('a').removeClass('active')
-        $(this).addClass('active');
-    })
+    }
+
+
 
 
 
